44c27092a8ab61e9942deeb415757955631de5a8
Pamela Silva <pamela.silva.qualitymap@araujo.com.br>
2025-03-18 17:58:31 -0300

    Condicionais simples e compostas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar as classes
+        // Exibir as mesnsagens para o usuário
+        //Obter pela scanner os valores digitados
+        // Exibir a mensagem criada 
     }
 }
