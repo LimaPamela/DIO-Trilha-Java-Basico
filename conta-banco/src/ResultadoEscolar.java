c12f6d52c7e7b01900d2e01136eba4466ad0ea2f
Pamela Silva <pamela.silva.qualitymap@araujo.com.br>
2025-03-18 19:28:33 -0300

    Java básico

--- conta-banco/src/ResultadoEscolar.java
+++ conta-banco/src/ResultadoEscolar.java
@@ -1,13 +1,19 @@
-
+//Condicional encadeada e ternária
 public class ResultadoEscolar {
-
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 7;
 
-        if(nota >=7)
-        System.out.println("Aprovado");
+    String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7  ? "Recuperação" : "Reprovado";
+    System.out.println(resultado);
 
-        else
-        System.out.println("Reprovado");
+
+        // if(nota >=7)
+        // System.out.println("Aprovado");
+
+        // else if (nota >= 5 && nota < 7)
+        // System.out.println("Prova Recuperação");
+        
+        // else
+        // System.out.println("Reprovado");
     }
 }
